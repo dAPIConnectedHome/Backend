f46f9d07b5768c233ca4d08b19740a743db5839f
Tobias Gilgenreiner <64074325+TobiasGilgenreiner@users.noreply.github.com>
2020-07-04 02:36:24 +0200

    Update publish automation

--- SSHTransferScript/SSHTransferScript/Program.cs
+++ SSHTransferScript/SSHTransferScript/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using WinSCP;
@@ -13,8 +14,20 @@ namespace SSHTransferScript
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] Args)
         {
+            #region Handel Commandline Arguments
+
+            if (Args.Length != 2)
+                throw new ArgumentException("Invalid number of arguments");
+
+            string sourcePath = Args[0];
+            string targetPath = Args[1];
+
+            #endregion //Handel Commandline Arguments
+
+            #region Password and certificate handling
+
             Console.Write("Enter Password for connection:");
             char[] password = new char[35];
             int passwordindex = 0;
@@ -39,50 +52,85 @@ namespace SSHTransferScript
             string Password =  new string(password);
 
             Console.WriteLine();
+            Console.Clear();
+
+            #endregion //Password and certificate handling
+
             try
             {
+                #region SSH session options
+
                 SessionOptions sessionOptions = new SessionOptions
                 {
                     Protocol = Protocol.Sftp,
-                    HostName = "192.168.0.10",
+                    HostName = "gitathome.dd-dns.de",
+                    PortNumber = 61998,
                     UserName = "pi",
                     Password = Password,
                     SshHostKeyFingerprint = "ssh-ed25519 255 adEE1YiHJUEcL3Qw57CXGLs20hRJFP/ArlftWYgA/aA="
                 };
 
+                #endregion //SSH session options
+
                 using (Session session = new Session())
                 {
-                    //Connect
+                    //Hook up Event
+                    session.FileTransferProgress += Session_FileTransferProgress;
+                    //Establish SSH connection
                     session.Open(sessionOptions);
 
-                    //Run PowerShell
-                    //RunspaceConfiguration runspaceConfiguration = RunspaceConfiguration.Create();
-                    //Runspace runspace = RunspaceFactory.CreateRunspace(runspaceConfiguration);
-                    //runspace.Open();
-                    //RunspaceInvoke scriptInvoker = new RunspaceInvoke(runspace);
-                    //
-                    //Pipeline pipeline = runspace.CreatePipeline();
-                    //
-                    //Command myCommand = new Command(Directory.GetCurrentDirectory() + @"\Powershellscript.ps1");
-                    //CommandParameter testParam = new CommandParameter("session", session);
-                    //myCommand.Parameters.Add(testParam);
-                    //
-                    //pipeline.Commands.Add(myCommand);
-                    //
-                    //var results = pipeline.Invoke();
-                    //
-                    //Console.WriteLine(results);
+                    #region DataTransfer
 
-                    ProcessStartInfo startInfo = new ProcessStartInfo();
-                    startInfo.FileName = "powershell.exe";
-                    startInfo.Arguments = Directory.GetCurrentDirectory() + @"\Powershellscript.ps1";
-                    startInfo.RedirectStandardOutput = true;
-                    startInfo.RedirectStandardError = true;
-                    startInfo.UseShellExecute = false;
-                    startInfo.CreateNoWindow = true;
-                    Process process = new Process();
-                    process.StartInfo = startInfo;
-                    process.Start();
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+                    
+                    TransferOperationResult transferResult;
+                    transferResult = session.PutFiles(@sourcePath, targetPath, false, transferOptions);
+
+                    transferResult.Check();
+
+                    if (_lastFileName != null)
+                    {
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write("{0}", _totalTransferedFilesCount);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(" Files transfered...");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("No Files transfered...");
+                    }
+
+                    #endregion //DataTransfer
+
+                    #region Make Software Runnable
+
+                    session.ExecuteCommand("chown pi " + targetPath + " -R").Check();
+
+                    session.ExecuteCommand("chmod 777 " + targetPath + " -R").Check();
+
+                    #endregion //Make Software Runnable
+
+                    #region Run Software
+
+                    Console.WriteLine("Do you want to Run the Application? (Y/n)");
+
+                    char response = '\0';
+                    while(response != 'Y')
+                    {
+                        response = Console.ReadKey().KeyChar;
+
+                        if (response == 'n')
+                            return;
+                    }
+
+                    session.ExecuteCommand("dotnet run " + "/home/pi/Desktop/DAPISmartHomeAppAPI/DapiSmartHomeMQTT_BackendServer");
+
+                    #endregion //Run Software
+
                 }
             }
             catch(Exception e)
@@ -91,11 +139,28 @@ namespace SSHTransferScript
             }
 
             finally
-            {
-                Console.WriteLine("Script done");
+            {   
                 Console.ReadKey();
             }
 
+        }
+
+        private static string _lastFileName;
+        private static int _totalTransferedFilesCount = 0;
+        private static void Session_FileTransferProgress(object sender, FileTransferProgressEventArgs e)
+        {
+            if((_lastFileName != null) && (_lastFileName != e.FileName))
+            {
+                Console.WriteLine();
+                ++_totalTransferedFilesCount;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\r{0:P0} ",e.OverallProgress);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("of transfer complete\t{0} ({1:P0})", e.FileName, e.FileProgress);
+
+            _lastFileName = e.FileName;
         }
     }
 }
--- SSHTransferScript/SSHTransferScript/bin/Debug/Powershellscript.ps1
+++ SSHTransferScript/SSHTransferScript/bin/Debug/Powershellscript.ps1
@@ -1,3 +1,15 @@
-$session = $args[0]
-$result = $session.PutFiles("$PSScriptRoot\bin\Debug\netcoreapp3.1\linux-arm\publish\*", "Desktop/TEst/").Check();
-write-host $param1
+Add-Type -Path "$PSScriptRoot\WinSCPnet.dll"
+
+
+Param(
+[paramter(Mandatory=$true)][object][ValidateNotNullOrEmpty()] $session
+)
+Param(
+[paramter(Mandatory=$true)][string[]][ValidateNotNullOrEmpty()] $publishlocation
+)
+Param(
+[paramter(Mandatory=$true)][string[]][ValidateNotNullOrEmpty()] $targetlocation
+)
+
+$result = $session.PutFiles("publishlocation", "targetlocation").Check();
+write-host $result
